5c3377a3394bd5540240b30e9d713dd3287b8b4e
Rohit ranjan <ranjanrohit812@gmail.com>
2021-11-14 23:41:14 +0530

    Init Commit

---
+++ cart.js
@@ -0,0 +1,31 @@
+const loadcart = () => {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "https://express-grocery-api.herokuapp.com/mycart", false);
+    xhttp.send();
+
+    const mycart = JSON.parse(xhttp.responseText);
+
+    for (let cart of mycart) {
+        const x = `
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${cart['grocery-name']}</h5>
+                        
+
+                        <div>Id: ${cart.productid}</div>
+                        <div>Price: ${cart.price}</div>
+                        <div>Total Quantity: ${cart.quantity}</div>
+
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        `
+
+        document.getElementById('cart').innerHTML = document.getElementById('cart').innerHTML + x;
+    }
+}
+
+loadcart();
---
+++ grocery.js
@@ -0,0 +1,31 @@
+const loadgrocery = () => {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "https://express-grocery-api.herokuapp.com/grocery", false);
+    xhttp.send();
+
+    const groceries = JSON.parse(xhttp.responseText);
+
+    for (let grocery of groceries) {
+        const x = `
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">${grocery['grocery-name']}</h5>
+                        
+
+                        <div>Id: ${grocery.productid}</div>
+                        <div>Price: ${grocery.price}</div>
+                        <div>Total Quantity: ${grocery.quantity}</div>
+
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        `
+
+        document.getElementById('grocery').innerHTML = document.getElementById('grocery').innerHTML + x;
+    }
+}
+
+loadgrocery();
